46624a9e4e82160667de22ef80d882da52afe445
André Fonseca <a39758@alunos.isel.pt>
2026-05-17 21:46:29 +0100

    Update head.php

--- rooted/views/partials/head.php
+++ rooted/views/partials/head.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Rooted</title>
     <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
+    <link rel="alternate" type="application/rss+xml" title="Rooted RSS Feed" href="/rss">
 </head>
 
 <body class="h-full">
