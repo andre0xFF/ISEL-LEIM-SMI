a23c65eed8180a488ac2d7cd822e3a18346339ee
André Fonseca <a39758@alunos.isel.pt>
2026-05-17 21:47:01 +0100

    Update create.php

--- rooted/Http/controllers/plants/create.php
+++ rooted/Http/controllers/plants/create.php
@@ -1,6 +1,13 @@
 <?php
 
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+$tags = $db->query("SELECT * FROM tags ORDER BY type, name")->get();
+
 view("plants/create.view.php", [
     "heading" => "Create Plant",
     "errors" => [],
+    "tags" => $tags,
 ]);
